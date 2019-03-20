aaccacb9c3a42c2fa0a3e86128adfd24f264e8c5
Florin Mirica, Jasmine Lee and Shawn Lunsford <shawn@>
2019-03-20 16:29:27 -0700

    near functional

---
+++ src/calculator.js
@@ -0,0 +1,64 @@
+export default class Calculator {
+  constructor(day, month, year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  getWeekday() {
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let weekdayCode = null;
+    let yearCode = this.getYearCode(this.year);
+    let monthCode = this.getMonthCode(this.month);
+    let centuryCode = this.getCenturyCode(this.year);
+    let leapYearCode = this.getLeapYearCode(this.year);
+
+    if(leapYearCode === 1 && (this.month === 1 || this.month === 2)){
+      weekdayCode = (yearCode + monthCode + centuryCode + this.day - leapYearCode) % 7 - 1;
+    } else {
+      weekdayCode = (yearCode + monthCode + centuryCode + this.day - leapYearCode) % 7;
+    }
+    let output = weekday[weekdayCode];
+    console.log(yearCode);
+    return output;
+  }
+
+  getYearCode() {
+    let yearCode = null;
+    if(this.year >= 1700 && this.year <= 1799) { yearCode = ((this.year - 1700) + ((this.year - 1700) / 4)) % 7; }
+    if(this.year >= 1800 && this.year <= 1899) { yearCode = ((this.year - 1800) + ((this.year - 1800) / 4)) % 7; }
+    if(this.year >= 1900 && this.year <= 1999) { yearCode = ((this.year - 1900) + ((this.year - 1900) / 4)) % 7; }
+    if(this.year >= 2000 && this.year <= 2099) { yearCode = ((this.year - 2000) + ((this.year - 2000) / 4)) % 7; }
+    if(this.year >= 2100 && this.year <= 2199) { yearCode = ((this.year - 2100) + ((this.year - 2100) / 4)) % 7; }
+    if(this.year >= 2200 && this.year <= 2299) { yearCode = ((this.year - 2200) + ((this.year - 2200) / 4)) % 7; }
+    if(this.year >= 2300 && this.year <= 2399) { yearCode = ((this.year - 2300) + ((this.year - 2300) / 4)) % 7; }
+    return yearCode;
+  }
+
+  getMonthCode() {
+    let monthCode = null;
+    if(this.month === 1 || this.month === 10){ monthCode = 0; }
+    if(this.month === 5) { monthCode = 1; }
+    if(this.month === 8) { monthCode = 2; }
+    if(this.month === 2 || this.month === 3 || this.month === 11) { monthCode = 3; }
+    if(this.month === 6) { monthCode = 4; }
+    if(this.month === 9 || this.month === 12) { monthCode = 5; }
+    if(this.month === 4 || this.month === 7) { monthCode = 6; }
+    return monthCode;
+  }
+
+  getCenturyCode() {
+    let centuryCode = null;
+    if((this.year >= 1900 && this.year <= 1999) || (this.year >= 2300 && this.year <= 2399)) { centuryCode = 0; }
+    if((this.year >= 1800 && this.year <= 1899) || (this.year >= 2200 && this.year <= 2299)) { centuryCode = 2; }
+    if((this.year >= 1700 && this.year <= 1799) || (this.year >= 2100 && this.year <= 2199)) { centuryCode = 4; }
+    if(this.year >= 2000 && this.year <= 2099) { centuryCode = 6; }
+    return centuryCode;
+  }
+
+  getLeapYearCode() {
+    let leapYearCode = 0;
+    if(this.year % 4 === 0 && (this.year % 100 !== 0 && this.year % 400 === 0)) { leapYearCode = 1; }
+    return leapYearCode;
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Calculator from './calculator.js';
+
+$(document).ready( function() {
+    $('form#calculator-form').submit(function(event){
+        event.preventDefault();
+        let dateInput = new Date($('#theDate').val());
+        let day = dateInput.getDate()+1;
+        let month = dateInput.getMonth()+1;
+        let year = dateInput.getFullYear();
+        let date = new Calculator(day, month, year);
+        let result = date.getWeekday();
+        console.log(result);
+        $('#output').prepend(result);
+    });
+});
